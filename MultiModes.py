d7b45084e483dd9acb1e5f8eb21a36d68fa5f748
javier-iaa <javier@iaa.es>
2022-07-21 17:14:18 +0200

    Update MultiModes.py
    Fixed bug in mean_flux: 'fluxes' referenced before assignment

--- MultiModes.py
+++ MultiModes.py
@@ -141,9 +141,10 @@ def lightcurve(file):
     T = time[-1]
     N = len(time)
     r = 1/T
-#    fluxes = np.array(data['PDCSAP_FLUX']) # comment for amplitude comparison with period04
+    fluxes = np.array(data['PDCSAP_FLUX'])
     mean_flux = np.mean(fluxes)
-    fluxes = (fluxes-mean_flux)/mean_flux*1000 # convert fluxes to mmag
+    # convert fluxes to mmag
+#   # fluxes = (fluxes-mean_flux)/mean_flux*1000  # comment for amplitude comparison with period04
     return time, fluxes, T, N, r
 
 def snr_or_fap(par):
